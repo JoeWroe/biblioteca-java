15407352dadbf4473deec44f49662667aa855b79
JoeWroe <joe_wroe@icloud.com>
2017-01-12 17:13:39 +0000

    Implement #Main

---
+++ src/BibliotecaApp.java
@@ -0,0 +1,10 @@
+/**
+ * Created by joe on 12/01/2017.
+ */
+public class BibliotecaApp {
+
+    public static void main(String[] args) {
+        WelcomeMessage welcomeMessage = new WelcomeMessage(System.out);
+        welcomeMessage.displayMessage();
+    }
+}
