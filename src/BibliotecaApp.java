aae5bfb0e23ec279617abc68b03b3bee74177f09
JoeWroe <joe_wroe@icloud.com>
2017-01-12 19:52:06 +0000

    Add bookLister to #main, Add #createBookList to BibliotecaApp
    ∆ Add #listAllBooks in order to print the book list.
    
    ∆ Add #createBookList for setting up the directory of books Biblioteca contains.

--- src/BibliotecaApp.java
+++ src/BibliotecaApp.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Contains #main for running the application.
  */
@@ -6,5 +9,17 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         WelcomeMessage welcomeMessage = new WelcomeMessage(System.out);
         welcomeMessage.displayMessage();
+        ArrayList<Book> bookList = createBookList();
+        BookLister bookLister = new BookLister(bookList);
+        bookLister.listAllBooks();
+    }
+
+    private static ArrayList<Book> createBookList() {
+        Book book1 = new Book("We Are All Completely Beside Ourselves");
+        Book book2 = new Book("Start With Why");
+        Book book3 = new Book("Steve Jobs: The Autobiography");
+        ArrayList<Book> bookList = new ArrayList<>();
+        bookList.addAll(Arrays.asList(book1, book2, book3));
+        return bookList;
     }
 }
